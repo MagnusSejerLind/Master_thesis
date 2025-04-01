ff47f43a3f4bb683b395ac119e97cb9ed0ce0a6c
MagnusSejerLind <magnussejerlind@gmail.com>
2025-04-01 16:15:58 +0200

    Create sensorPlacement.m

---
+++ sensorPlacement.m
@@ -0,0 +1,124 @@
+clc,clear,
+close all
+set(0,'defaultTextInterpreter','latex');
+rng('default')
+opt.plot = 1;           % [0/1]
+opt.larm = 1;
+%% System properties
+
+
+optPlace = zeros(25,2);
+for  numcodcount= 1:25
+numcodcount
+    opt.numDOF = numcodcount;
+
+noOut = 2;  % Number of outputs (sensors)
+
+opt.sysType = "chain";  % ["chain"] - Type of system
+% opt.method = "TA";      % ["TA"/"ME"] - Virtuel sensing method (Toeplitz's/Modal expansion)
+opt.out_type = 2;       % [disp=0 / vel=1 / acc=2] - Define output type
+opt.error_mod = 0;      % [0/1] - Include error modeling and noise
+% opt.nonlinear = 0;      % [0/1] - Include nonlinearties in the system
+% opt.nonlinType = 0;     % [0=constant / 1=varied] - Define type of nonlineaties
+
+% opt.numDOF = 16          % Number of DOF
+
+% in_dof = [1 3];         % Input DOF
+
+
+%% System modeling
+
+[dof,m,k,xi] = systemSetup(opt);
+
+% Time
+N = 500;
+dt = 0.01;
+t = 0:dt:(N-1)*dt;
+
+% Base system
+if opt.error_mod == 1; [k,m,snr] = modeling_error(k,m); end
+[M,~,K] = chain(m,m*0,k,dof);
+[Phi,Lambda] = eig(K,M);    % modal and spectral matrix
+[omegaN,i2] = sort(sqrt(diag(Lambda))); % Natural freq.
+omegaN = real(omegaN);
+Phi=Phi(:,i2);
+dd = sqrt(diag(Phi'*M*Phi));
+aa = Phi*diag(1./dd);    % Mass-normalized Phi (eigenvec.)
+C_modal = diag(2*xi.*omegaN);
+C = inv(aa)'*C_modal*inv(aa);
+
+
+% Extended system ---------not defined same as in outputEst_general (as of 28/3)
+in_dof_ex = (1:1:dof);
+r_ex = numel(in_dof_ex);
+ms_ex = noOut;
+
+
+if noOut == 1
+    condNum = zeros(1,dof);
+    for i = 1:dof
+
+        out_dof_ex = i;
+
+        [Ad_ex,Bd_ex,Cd_ex,Dd_ex] = systemMatriciesSS_dis(M,K,C,dof,in_dof_ex,out_dof_ex,opt.out_type,dt);
+        [H_ex] = TeoplitzMatrix(N,ms_ex,r_ex,Ad_ex,Bd_ex,Cd_ex,Dd_ex);
+        condNum(i) = cond(H_ex);    % condtion of H_ex:
+    end
+
+    [~, minIdx] = min(condNum);
+    fprintf('Optimal sensor placement: DOF %g\n',minIdx)
+end
+
+
+if noOut == 2
+    condNum = NaN(dof);
+disp("Completion:")
+    for i = 1:dof       
+        for j = 1:dof
+            if i ~= j   % Skip i=j cases
+                
+                out_dof_ex = [i,j];
+                [Ad_ex,Bd_ex,Cd_ex,Dd_ex] = systemMatriciesSS_dis(M,K,C,dof,in_dof_ex,out_dof_ex,opt.out_type,dt);
+                [H_ex] = TeoplitzMatrix(N,ms_ex,r_ex,Ad_ex,Bd_ex,Cd_ex,Dd_ex);
+                condNum(i,j) = cond(H_ex);    % condtion of H_ex:
+            end
+        end
+        disp((i/dof))
+    end
+    [minValue, linearIdx] = min(condNum(:));
+    [rowIdx, colIdx] = ind2sub(size(condNum), linearIdx);
+    fprintf('Optimal sensor placement: DOF %g,%g\n',rowIdx,colIdx)
+end
+
+
+optPlace(dof,1) = rowIdx;
+optPlace(dof,2) = colIdx;
+
+
+end
+if opt.larm == 1
+load gong
+sound(y,Fs)
+end
+
+% ikke korrekt (og grim)
+
+
+% evt. condNum(i+j+k)
+
+% if noOut == 3
+%     count =0
+%     for i = 1:dof
+%         for j = 1:dof
+%             for k = 1:dof
+%                 count = count+k
+%                 out_dof_ex = [i,j,k]
+%                 [Ad_ex,Bd_ex,Cd_ex,Dd_ex] = systemMatriciesSS_dis(M,K,C,dof,in_dof_ex,out_dof_ex,opt.out_type,dt);
+%                 [H_ex] = TeoplitzMatrix(N,ms_ex,r_ex,Ad_ex,Bd_ex,Cd_ex,Dd_ex);
+%
+%                  condNum(count) = cond(H_ex)
+%
+%             end
+%         end
+%     end
+% end
