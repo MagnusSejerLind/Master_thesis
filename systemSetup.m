e745e3c6f08ce68c34b3d46dbce34671e57b1762
MagnusSejerLind <magnussejerlind@gmail.com>
2025-04-04 13:52:03 +0200

    Update systemSetup.m

--- systemSetup.m
+++ systemSetup.m
@@ -5,7 +5,7 @@ if opt.sysType == "chain"
     dof = opt.numDOF;
     m = ones(1,dof)*1;
     k = ones(1,dof)*250;
-    xi = (ones(1,dof)*0.1)';
+    xi = (ones(1,dof)*0.01)';
 
 else 
     print('System not defined')
