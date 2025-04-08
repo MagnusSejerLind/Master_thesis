3fa94a1a9d7a90488d9770d8d8068709c016c35e
MagnusSejerLind <magnussejerlind@gmail.com>
2025-04-08 19:51:25 +0200

    Update systemSetup.m

--- systemSetup.m
+++ systemSetup.m
@@ -1,12 +1,26 @@
 function [dof,m,k,xi] = systemSetup(opt)
 
+xi = 0.01;
+
+
 if opt.sysType == "chain"
 
     dof = opt.numDOF;
     m = ones(1,dof)*1;
     k = ones(1,dof)*250;
-    xi = (ones(1,dof)*0.01)';
+    xi = (ones(1,dof)*xi)';
 
-else 
-    print('System not defined')
+elseif opt.sysType ==  "frame"
+
+[~, ~, ~, ~, ~, dof, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~] = loadstructure;
+
+
+    xi = (ones(1,dof)*xi)';
+
+
+    m = [];
+    k = [];
+
+else
+    print('System correctly not defined')
 end
