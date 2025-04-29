1ed48a9462f7af3e26bb7658f5014f3111ab7ff0
MagnusSejerLind <magnussejerlind@gmail.com>
2025-04-29 16:37:58 +0200

    Update sensorPlacement.m

--- sensorPlacement.m
+++ sensorPlacement.m
@@ -55,136 +55,32 @@ ms_ex = noOut;
 
 %% chain
 if opt.sysType == "chain"
-% m=1
-if noOut == 1
-    condNum = NaN(1,dof);
-    for i = 1:dof
+    % m=1
+    if noOut == 1
+        condNum = NaN(1,dof);
+        for i = 1:dof
 
-        out_dof_ex = i;
+            out_dof_ex = i;
 
-        [Ad_ex,Bd_ex,Cd_ex,Dd_ex] = systemMatriciesSS_dis(M,K,C,dof,in_dof_ex,out_dof_ex,opt.out_type,dt);
-        [H_ex] = TeoplitzMatrix(N,ms_ex,r_ex,Ad_ex,Bd_ex,Cd_ex,Dd_ex);
-        condNum(i) = cond(H_ex);    % condtion of H_ex:
-    end
-
-    [~, minIdx] = min(condNum);
-
-    % fprintf('Optimal sensor placement: DOF %g\n',minIdx)
-    optPlace(dof) = minIdx;
-end
-
-
-
-
-
-% m=2
-%%% cases med (i,j)=(j,i)' kan skibbes (halver calcu)
-if noOut == 2
-    condNum = NaN(dof);
-    disp("Completion:")
-    for i = 1:dof
-        for j = 1:dof
-            if i ~= j   % Skip i=j cases
-
-                out_dof_ex = [i,j];
-                [Ad_ex,Bd_ex,Cd_ex,Dd_ex] = systemMatriciesSS_dis(M,K,C,dof,in_dof_ex,out_dof_ex,opt.out_type,dt);
-                [H_ex] = TeoplitzMatrix(N,ms_ex,r_ex,Ad_ex,Bd_ex,Cd_ex,Dd_ex);
-                condNum(i,j) = cond(H_ex);    % condtion of H_ex:
-            end
+            [Ad_ex,Bd_ex,Cd_ex,Dd_ex] = systemMatriciesSS_dis(M,K,C,dof,in_dof_ex,out_dof_ex,opt.out_type,dt);
+            [H_ex] = TeoplitzMatrix(N,ms_ex,r_ex,Ad_ex,Bd_ex,Cd_ex,Dd_ex);
+            condNum(i) = cond(H_ex);    % condtion of H_ex:
         end
-        disp((i/dof))
-    end
-    [minValue, linearIdx] = min(condNum(:));
-    [rowIdx, colIdx] = ind2sub(size(condNum), linearIdx);
-    fprintf('Optimal sensor placement: DOF %g,%g\n',rowIdx,colIdx)
 
-    optPlace(dof,1) = rowIdx;
-    optPlace(dof,2) = colIdx;
-end
+        [~, minIdx] = min(condNum);
 
-% m=3
-if noOut == 3
-    count =0;
-    for i = 1:dof
-        for j = 1:dof
-            if i ~= j
-                for k = 1:dof
-                    if k ~= i
-                        if k ~= j
-                            count = count+1;
-                            out_dof_ex = [i,j,k];
-                            [Ad_ex,Bd_ex,Cd_ex,Dd_ex] = systemMatriciesSS_dis(M,K,C,dof,in_dof_ex,out_dof_ex,opt.out_type,dt);
-                            [H_ex] = TeoplitzMatrix(N,ms_ex,r_ex,Ad_ex,Bd_ex,Cd_ex,Dd_ex);
-
-                            condNum(numcodcount-2,count) = cond(H_ex);
-                        end
-                    end
-                end
-            end
-        end
-    end
-end
-
-
-
-end
-
-
-%% Beam frame
-
-if opt.sysType == "frame"
-
-if noOut == 1
-
-    for i = 1:dof
-        out_dof_ex = i
-
-        [Ad_ex,Bd_ex,Cd_ex,Dd_ex] = systemMatriciesSS_dis(M,K,C,dof,in_dof_ex,out_dof_ex,opt.out_type,dt);
-        [H_ex] = TeoplitzMatrix(N,ms_ex,r_ex,Ad_ex,Bd_ex,Cd_ex,Dd_ex);
-
-        condNum(i) = cond(H_ex);    % condtion of H_ex:
-
+        % fprintf('Optimal sensor placement: DOF %g\n',minIdx)
+        optPlace(dof) = minIdx;
     end
 
-    [~, minIdx] = min(condNum);
-    optPlace = minIdx; % OBS. fixed dof not included
-
-
-    % Determine the node and DOF type
-    node = ceil(optPlace / 3);
-    dofTypeIndex = mod(optPlace - 1, 3);
-
-    switch dofTypeIndex
-        case 0
-            dofType = 'x';
-        case 1
-            dofType = 'y';
-        case 2
-            dofType = '\theta';
-    end
-    fprintf('DOF %d is located at node %d and corresponds to the %s degree of freedom.\n', optPlace, node, dofType);
-
-if opt.plot == 1
-    figure()
-    hold on
-    plot(condNum(1:3:end),'.--',LineWidth=1,MarkerSize=30)
-    plot(condNum(2:3:end),'.--',LineWidth=1,MarkerSize=30)
-    plot(condNum(3:3:end),'.--',LineWidth=1,MarkerSize=30)
-    legend('x','y','\theta')
-    title('Condition number of DOFs - Single output')
-    xlabel('Node')
-    ylabel('Condition number')
-    grid 
-end
 
 
 
-end
 
-
-
-if noOut == 2
-condNum = NaN(dof);
+    % m=2
+    %%% cases med (i,j)=(j,i)' kan skibbes (halver calcu)
+    if noOut == 2
+        condNum = NaN(dof);
         disp("Completion:")
         for i = 1:dof
             for j = 1:dof
@@ -201,14 +97,125 @@ condNum = NaN(dof);
         [minValue, linearIdx] = min(condNum(:));
         [rowIdx, colIdx] = ind2sub(size(condNum), linearIdx);
         fprintf('Optimal sensor placement: DOF %g,%g\n',rowIdx,colIdx)
-    
-    optPlace(dof,1) = rowIdx;
-    optPlace(dof,2) = colIdx;
 
-    surf(condNum)
+        optPlace(dof,1) = rowIdx;
+        optPlace(dof,2) = colIdx;
+
+
+        if opt.plot == 1
+            figure()
+            surf(condNum)
+            colormap('parula');
+            grid minor
+            xlim([0 25])
+            ylim([0 25])
+            ylabel('DOF $i$')
+            xlabel('DOF $j$')
+            zlabel('$\kappa \left( \bar{H} \right)$')
+            title('Condition number sensor placement - m=2')
+        end
+    end
+
+
+
+    % m=3
+    if noOut == 3
+        count =0;
+        for i = 1:dof
+            for j = 1:dof
+                if i ~= j
+                    for k = 1:dof
+                        if k ~= i
+                            if k ~= j
+                                count = count+1;
+                                out_dof_ex = [i,j,k];
+                                [Ad_ex,Bd_ex,Cd_ex,Dd_ex] = systemMatriciesSS_dis(M,K,C,dof,in_dof_ex,out_dof_ex,opt.out_type,dt);
+                                [H_ex] = TeoplitzMatrix(N,ms_ex,r_ex,Ad_ex,Bd_ex,Cd_ex,Dd_ex);
+
+                                condNum(numcodcount-2,count) = cond(H_ex);
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+
+
 end
 
 
+%% Beam frame
+
+if opt.sysType == "frame"
+
+    if noOut == 1
+
+        for i = 1:dof
+            out_dof_ex = i
+            [Ad_ex,Bd_ex,Cd_ex,Dd_ex] = systemMatriciesSS_dis(M,K,C,dof,in_dof_ex,out_dof_ex,opt.out_type,dt);
+            [H_ex] = TeoplitzMatrix(N,ms_ex,r_ex,Ad_ex,Bd_ex,Cd_ex,Dd_ex);
+            condNum(i) = cond(H_ex);    % condtion of H_ex:
+        end
+        [~, minIdx] = min(condNum);
+        optPlace = minIdx; % OBS. fixed dof not included
+
+        % Determine the node and DOF type
+        node = ceil(optPlace / 3);
+        dofTypeIndex = mod(optPlace - 1, 3);
+
+        switch dofTypeIndex
+            case 0
+                dofType = 'x';
+            case 1
+                dofType = 'y';
+            case 2
+                dofType = '\theta';
+        end
+        fprintf('DOF %d is located at node %d and corresponds to the %s degree of freedom.\n', optPlace, node, dofType);
+
+        if opt.plot == 1
+            figure()
+            hold on
+            plot(condNum(1:3:end),'.--',LineWidth=1,MarkerSize=30)
+            plot(condNum(2:3:end),'.--',LineWidth=1,MarkerSize=30)
+            plot(condNum(3:3:end),'.--',LineWidth=1,MarkerSize=30)
+            legend('x','y','\theta')
+            title('Condition number overs Frame structre DOFs - $m=1$')
+            xlabel('Node')
+            ylabel('$\kappa \left( \bar{H} \right)$')
+            grid
+            ylim([0 6500])
+        end
+    end
+
+
+
+    if noOut == 2
+        condNum = NaN(dof);
+        disp("Completion:")
+        for i = 1:dof
+            for j = 1:dof
+                if i ~= j   % Skip i=j cases
+
+                    out_dof_ex = [i,j];
+                    [Ad_ex,Bd_ex,Cd_ex,Dd_ex] = systemMatriciesSS_dis(M,K,C,dof,in_dof_ex,out_dof_ex,opt.out_type,dt);
+                    [H_ex] = TeoplitzMatrix(N,ms_ex,r_ex,Ad_ex,Bd_ex,Cd_ex,Dd_ex);
+                    condNum(i,j) = cond(H_ex);    % condtion of H_ex:
+                end
+            end
+            disp((i/dof))
+        end
+        [minValue, linearIdx] = min(condNum(:));
+        [rowIdx, colIdx] = ind2sub(size(condNum), linearIdx);
+        fprintf('Optimal sensor placement: DOF %g,%g\n',rowIdx,colIdx)
+
+        optPlace(dof,1) = rowIdx;
+        optPlace(dof,2) = colIdx;
+
+        surf(condNum)
+    end
 
 end
 %%
