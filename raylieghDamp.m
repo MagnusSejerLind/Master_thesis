2bc402069c80ad4d56d8af637597094875417440
MagnusSejerLind <magnussejerlind@gmail.com>
2025-05-08 19:33:50 +0200

    Update raylieghDamp.m

--- raylieghDamp.m
+++ raylieghDamp.m
@@ -1,8 +1,8 @@
 function [alpha, beta] = raylieghDamp(omegaN,xi)
+% Calculates the Rayleigh damping coeffcient
 
-
-A = [1, omegaN(1)^2 ; 1 omegaN(2)^2]
+A = [1, omegaN(1)^2 ; 1 omegaN(2)^2];
 b = [2*xi(1)*omegaN(1) ; 2*xi(2)*omegaN(2)];
 x = A \ b;
-alpha = x(1)
-beta = x(2)
+alpha = x(1);
+beta = x(2);
