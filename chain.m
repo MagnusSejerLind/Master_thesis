86eab10260ee1d5ef7aa3ab7e3762b737aaac60a
MagnusSejerLind <magnussejerlind@gmail.com>
2025-05-08 19:32:09 +0200

    Update chain.m

--- chain.m
+++ chain.m
@@ -1,22 +1,22 @@
-function [M,C,K]=chain(m1,c1,k1,dof)
+function [M,C,K] = chain(m,c,k,dof)
 % Expresses system matricies of a chain system
 
 
 if dof~=1
     for j=1:dof-1
-        kd(j) = k1(j)+k1(j+1);
-        cd(j) = c1(j)+c1(j+1);
-        kf(j) = -k1(j+1);
-        cf(j) = -c1(j+1);
+        kd(j) = k(j)+k(j+1);
+        cd(j) = c(j)+c(j+1);
+        kf(j) = -k(j+1);
+        cf(j) = -c(j+1);
     end
-    kd(dof) = k1(dof);
-    cd(dof) = c1(dof);
+    kd(dof) = k(dof);
+    cd(dof) = c(dof);
     K = diag(kf,1)+diag(kf,-1)+diag(kd);
     C = (diag(cf,1)+diag(cf,-1)+diag(cd));
-    M = diag(m1);
+    M = diag(m);
 else
-    K = k1;
-    M = m1;
-    C = c1;
+    K = k;
+    M = m;
+    C = c;
 end
 end
