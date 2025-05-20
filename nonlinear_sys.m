ecd0d789279ca3532c1b608bebc71415385942a4
MagnusSejerLind <magnussejerlind@gmail.com>
2025-05-20 17:34:59 +0200

    Update nonlinear_sys.m

--- nonlinear_sys.m
+++ nonlinear_sys.m
@@ -4,6 +4,8 @@
 
 %%%%%
 % OBS REMOVE ERRORS AND NOISE
+
+% -gen. update req.
 %%%%%
 
 
@@ -17,7 +19,7 @@ set(0,'defaultTextInterpreter','latex');
 
 opt.sysType = "chain";
 opt.numDOF = 4;          % Number of DOF
-[dof,m,k,xi] = systemSetup(opt);
+[dof,m,k,xi_int] = systemSetup(opt);
 
 out_dof = [1 3];
 out_type = 0;   % disp=0, vel=1, acc=2
@@ -42,8 +44,11 @@ omegaN_acc = real(omegaN_acc);
 Phi_acc=Phi_acc(:,i2);
 dd_acc = sqrt(diag(Phi_acc'*M_acc*Phi_acc));
 aa_acc = Phi_acc*diag(1./dd_acc);    % Mass-normalized Phi (eigenvec.)
-C_modal_acc = diag(2*xi.*omegaN_acc);
-C_acc = inv(aa_acc)'*C_modal_acc*inv(aa_acc);
+[alpha_acc,beta_acc] = raylieghDamp(omegaN_acc,xi_int);
+C_acc = alpha_acc*M_acc + beta_acc*K_acc;
+C_modal_acc = round(Phi_acc'*C_acc*Phi_acc,10);
+% C_modal_acc = diag(2*xi.*omegaN_acc);
+% C_acc = inv(aa_acc)'*C_modal_acc*inv(aa_acc);
 
 % Model errors
 [k,m,snr] = modeling_error(k,m);
@@ -54,8 +59,10 @@ omegaN = real(omegaN);
 Phi=Phi(:,i2);
 dd = sqrt(diag(Phi'*M*Phi));
 aa = Phi*diag(1./dd);    % Mass-normalized Phi (eigenvec.)
-C_modal = diag(2*xi.*omegaN);
-C = inv(aa)'*C_modal*inv(aa);   % Damping matrix
+[alpha,beta] = raylieghDamp(omegaN,xi_int);
+C = alpha*M + beta*K;
+C_modal = round(Phi'*C*Phi,10);
+xi = diag(C_modal) ./ (2*omegaN);
 
 % System matricies S-S
 [Ad_acc,Bd_acc,Cd_acc,Dd_acc] = systemMatriciesSS_dis(M_acc,K_acc,C_acc,dof,in_dof_ex,out_dof_ex,out_type,dt);
@@ -71,12 +78,12 @@ N = 500;
 t = 0:dt:(N-1)*dt;
 
 % Input (nodes defined earlier)
-u_mag = 100;
-% u = ones(r,N)*u_mag;
-% u = u.*sin(t*10);
+u_mag = 10;
+u = ones(r,N)*u_mag;
+u = u.*sin(t*5);
 % u = zeros(r,N);
-u = ones(r,N);
-u(N*0.2) = u_mag;
+% u = ones(r,N);
+% u(N*0.2) = u_mag;
 U = u(:);
 
 
@@ -119,8 +126,8 @@ end
 
 %% nonlinear extended system
 
-cf_nl = 0.01;    % coeffcient of nonlinear damping force
-kf_nl = 0.1;    % coeffcient of nonlinear stiffness force
+cf_nl = 0.5;    % coeffcient of nonlinear damping force
+kf_nl = 0.5;    % coeffcient of nonlinear stiffness force
 
 
 [Ad_ex,Bd_ex,Cd_ex,Dd_ex] = systemMatriciesSS_dis(M_acc,K_acc,C_acc,dof,in_dof_ex,out_dof_ex,out_type,dt);
@@ -134,7 +141,7 @@ for i = 1:N
     fd_nl_ex(dof+1:end) = cf_nl*z_old_ex(dof+1:end).*abs(z_old_ex(dof+1:end));   % non-linear damping force (velocity dependt)
     fk_nl_ex(dof+1:end)  = kf_nl*(z_old_ex(1:dof).^3);    % non-linear stiffness force (displacement dependt)
 
-    z_new_ex = Ad_ex*z_old_ex + Bd_ex*u(:,i) - fd_nl_ex + fk_nl_ex;
+    z_new_ex = Ad_ex*z_old_ex + Bd_ex*u(:,i) - fd_nl_ex - fk_nl_ex;
     y_ex(:,i) = Cd_ex*z_old_ex + Dd_ex*u(:,i);
     z_old_ex = z_new_ex;
 end
@@ -158,7 +165,7 @@ title('Output: linear/nonlinear')
 %   Y_ex: nonlinear extended (ms=4,r=2), Y_acc: LTI extended, Y: LTI
 
     
-[H] = TeoplitzMatrix(N,ms,r,Ad,Bd,Cd,Dd);  % (H tilde)
+[H] = ToeplitzMatrix(N,ms,r,Ad,Bd,Cd,Dd);  % (H tilde)
 % [H_ex] = TeoplitzMatrix(N,ms_ex,r_ex,Ad_ex,Bd_ex,Cd_ex,Dd_ex);  
 
 %% System: m output, n input (H hat): _con {converter}
@@ -179,12 +186,16 @@ omegaN_con = real(omegaN_con);
 Phi_con=Phi_con(:,i2);
 dd_con = sqrt(diag(Phi_con'*M_con*Phi_con));
 aa_con = Phi_con*diag(1./dd_con);    % Mass-normalized Phi (eigenvec.)
-C_modal_con = diag(2*xi_con.*omegaN_con);
-C_con = inv(aa_con)'*C_modal_con*inv(aa_con);   % Damping matrix
+[alpha,beta] = raylieghDamp(omegaN,xi_int);
+C_con = alpha*M + beta*K;
+C_modal_con = round(Phi'*C*Phi,10); % Damping matrix
+xi = diag(C_modal) ./ (2*omegaN);   
+
+
 
 [Ad_con,Bd_con,Cd_con,Dd_con] = systemMatriciesSS_dis(M_con,K_con,C_con,dof,in_dof_con,out_dof_con,out_type,dt);
 
-[H_con] = TeoplitzMatrix(N,ms_con,r_con,Ad_con,Bd_con,Cd_con,Dd_con);
+[H_con] = ToeplitzMatrix(N,ms_con,r_con,Ad_con,Bd_con,Cd_con,Dd_con);
 
 %%
 
