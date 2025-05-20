09ee9cc70d38d7690bb9202a97407e09758fa710
MagnusSejerLind <magnussejerlind@gmail.com>
2025-05-20 17:34:34 +0200

    Update ToeplitzMatrix.m

--- ToeplitzMatrix.m
+++ ToeplitzMatrix.m
@@ -6,9 +6,9 @@ function [H_N] = ToeplitzMatrix(N,ms,r,Ad,Bd,Cd,Dd)
 % r: no. of input dof
 % Ad,Bd,Cd,Dd: SS system matricies
 
-H_N=[];
-for jj=1:N
-    if jj==1
+H_N = [];
+for jj = 1:N
+    if jj == 1
         H_N((jj-1)*ms+1:jj*ms,1:jj*r)=[Dd H_N];
     else
         Q=Cd*Ad^((jj-1)-1)*Bd;
