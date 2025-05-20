abc945ca850bb98e4b18c781ff6ff1ba92b71492
MagnusSejerLind <magnussejerlind@gmail.com>
2025-05-20 17:35:17 +0200

    Update main.m

--- main.m
+++ main.m
@@ -3,28 +3,28 @@ close all
 set(0,'defaultTextInterpreter','latex');
 rng('default')
 
-%% System properties & options
-opt.sysType = "frame";  % ["chain" / "frame"] - Type of system
+%% System & options
+opt.sysType = "chain";  % ["chain" / "frame"] - Type of system
 opt.method = "TA";      % ["TA"/"ME"] - Virtuel sensing method (Toeplitz's/Modal expansion)
-opt.out_type = 2;       % [0/1/2] - Define output type (0=disp, 1=vel, 2=accel)
+opt.out_type = 0;       % [0/1/2] - Define output type (0=disp, 1=vel, 2=accel)
+opt.numDOF = 6;         % [-int.-] - Number of DOF --ONLY FOR CHAIN SYSTEM
+opt.nonlinear = 0;      % [0/1] - Include nonlinearties in the system
+opt.nonlinType = 1;     % [0/1] - Define type of nonlineaties (0=constant / 1=varied)
 opt.error_mod = 0;      % [0/1] - Include error modeling and noise
-opt.nonlinear = 1;      % [0/1] - Include nonlinearties in the system
-opt.nonlinType = 1;     % [0=constant / 1=varied] - Define type of nonlineaties
-opt.numDOF = 8;         % [-int.-] - Number of DOF --ONLY FOR CHAIN SYSTEM
 opt.psLoads = 1;        % [0/1] - Apply pseodu loads to convert nonlinear system to linear model
-opt.plot = 0;           % [0/1] - plots results
+opt.condimp = 1;        % [0/1] - Improve Toeplitz's matrix condition by truncation
+opt.plot = 1;           % [0/1] - plot results
 opt.animate = 0;        % [0/1] - Animates the displacements of the structure
 opt.aniSave = 0;        % [0/1] - Save animation
-opt.condimp = 1;        % [0/1] - Improve Toeplitz's matrix condition
 opt
 
-in_dof = [1 4 5];         % Input DOF
-out_dof = [1 2 3];        % Output DOF
+in_dof = [1];         % Input DOF
+out_dof = [2 5];        % Output DOF
 % out_dof = [1 2 3 4 5 6 7 8 9 10 12 15 16 18 19 20 22 23 24];        % Output DOF
 % frame dof: (x,y,θ)
 %% System modeling
 
-[dof,m,k,xi] = systemSetup(opt);
+[dof,m,k,xi_int] = systemSetup(opt);
 r = numel(in_dof);
 ms = numel(out_dof);
 
@@ -34,7 +34,7 @@ v0 = zeros(dof,1);
 z0 = [d0;v0];
 
 % Time
-N = 300;
+N = 500;
 dt = 0.01;
 t = 0:dt:(N-1)*dt;
 
@@ -63,7 +63,7 @@ omegaN_acc = real(omegaN_acc);
 Phi_acc = Phi_acc(:,i2);
 dd_acc = sqrt(diag(Phi_acc'*M_acc*Phi_acc));
 aa_acc = Phi_acc*diag(1./dd_acc);    % Mass-normalized Phi (eigenvec.)
-[alpha_acc,beta_acc] = raylieghDamp(omegaN_acc,xi);
+[alpha_acc,beta_acc] = raylieghDamp(omegaN_acc,xi_int);
 C_acc = alpha_acc*M_acc + beta_acc*K_acc;
 C_modal_acc = round(Phi_acc'*C_acc*Phi_acc,10);
 
@@ -81,10 +81,11 @@ omegaN = real(omegaN);
 Phi = Phi(:,i2);
 dd = sqrt(diag(Phi'*M*Phi)); % Mass norm M
 aa = Phi*diag(1./dd);    % Mass-normalized Phi (eigenvec.)
-[alpha,beta] = raylieghDamp(omegaN,xi);
+[alpha,beta] = raylieghDamp(omegaN,xi_int);
 C = alpha*M + beta*K;
 C_modal = round(Phi'*C*Phi,10);
 xi = diag(C_modal) ./ (2*omegaN);
+
 
 % Extended system - full output
 in_dof_ex = in_dof;
@@ -92,6 +93,7 @@ out_dof_ex = (1:1:dof);
 dof_ex = numel(out_dof_ex);
 r_ex = numel(in_dof_ex);
 ms_ex = numel(out_dof_ex);
+
 
 % System matricies
 [Ad,Bd,Cd,Dd] = systemMatriciesSS_dis(M,K,C,dof,in_dof,out_dof,opt.out_type,dt);
@@ -125,6 +127,7 @@ z_old = z0;
 z_new = zeros(size(z_old));
 fd_nl = zeros(size(z_old));
 fk_nl = zeros(size(z_old));
+y = zeros(ms,length(t));
 for i = 1:N
     fd_nl(dof+1:end) = cf_nl*z_old(dof+1:end).*abs(z_old(dof+1:end));   % non-linear damping force (velocity dependent)
     fk_nl(dof+1:end)  = kf_nl*(z_old(1:dof).^3);                        % non-linear stiffness force (displacement dependent)
@@ -135,6 +138,7 @@ for i = 1:N
 end
 Y = y(:);
 if opt.error_mod == 1
+% output noise
     Y = awgn(Y,snr,'measured');
     y = reshape(Y,ms,N);
 end
@@ -142,35 +146,29 @@ end
 % Actual system
 z_old_acc = z0;
 z_new_acc = zeros(size(z_old_acc));
-
 fd_nl_acc = zeros(size(z_old_acc));
 fk_nl_acc = zeros(size(z_old_acc));
+y_acc = zeros(dof,length(t));
 for i = 1:N
     fd_nl_acc(dof+1:end) = cf_nl*z_old_acc(dof+1:end).*abs(z_old_acc(dof+1:end));   % non-linear damping force (velocity dependt)
     fk_nl_acc(dof+1:end)  = kf_nl*(z_old_acc(1:dof).^3);                            % non-linear stiffness force (displacement dependt)
 
-    z_new_acc = Ad_acc*z_old_acc + Bd_acc*u(:,i) - fd_nl_acc + fk_nl_acc;
+    z_new_acc = Ad_acc*z_old_acc + Bd_acc*u(:,i) - fd_nl_acc - fk_nl_acc;
     y_acc(:,i) = Cd_acc*z_old_acc + Dd_acc*u(:,i);
     z_old_acc = z_new_acc;
 end
 Y_acc = y_acc(:);
 
-%% Condion improvement
+%% Condition improvement
 
 if opt.condimp == 1
-
-% cn = cond(H)
-% cn_ex = cond(H_ex)
+% Removes first ms rows, and last r columns in the Toeplitz's matricies
 
 H = H(ms+1:end, 1:end-r);
 H_ex = H_ex(ms_ex+1:end, 1:end-r_ex);
 
 U = U(1:end-r);
 Y = Y(ms+1:end);
-
-
-% cn = cond(H)
-% cn_ex = cond(H_ex)
 
 end
 
@@ -184,13 +182,25 @@ if opt.psLoads == 1
     [Ad_FI,Bd_FI,Cd_FI,Dd_FI] = systemMatriciesSS_dis(M,K,C,dof,in_dof_FI,out_dof,opt.out_type,dt);
     [H_FI] = ToeplitzMatrix(N,ms,r_FI,Ad_FI,Bd_FI,Cd_FI,Dd_FI);
 
-if opt.condimp
-H_FI = H_FI(ms+1:end, 1:end-r_FI);
-end
+    if opt.condimp
+        H_FI = H_FI(ms+1:end, 1:end-r_FI);
+    end
+
+    % % Tikhonov regularization parameter
+    % lambda = 0.071969;                   % adjust this to trade bias vs. variance
+    % % lambda = 1e-10;
+    % % build regularized normal equations
+    % A = H_FI' * H_FI + lambda * eye(size(H_FI,2));
+    % b = H_FI' * (Y - H * U);
+    % % solve for Gamma
+    % Gamma = A \ b;
 
     Gamma = pinv(H_FI)*(Y - H*U);
+
+
 else
     Gamma = zeros(dof*length(t),1);
+    if opt.condimp == 1 && opt.psLoads ~= 1; Gamma = Gamma(1:end-dof); end
 end
 %% Output estimation
 
@@ -241,8 +251,9 @@ mu1 = out_dof;              % Observed nodes
 mu2 = 1:dof; mu2(mu1)=[];   % Unobserved nodes
 
 if opt.condimp == 1
-y_acc = y_acc(:,2:end);
-t = t(2:end);
+    y_acc = y_acc(:,2:end);
+    Y_acc = y_acc(:);
+    t = t(2:end);
 end
 
 % Root mean squared error
@@ -255,32 +266,32 @@ RMSE_tot = mean(RMSE)
 
 if opt.plot == 1
     figure()
-bar(mu2,RMSE,'k')
-xlabel('DOF $(x,y,\theta )$')
-ylabel('RMSE')
-title('Root mean squared error')
-grid 
-xticks(1:1:dof)
+    bar(mu2,RMSE,'k')
+    xlabel('DOF $(x,y,\theta )$')
+    ylabel('RMSE')
+    title('Root mean squared error')
+    grid
+    xticks(1:1:dof)
 end
 
 % RMSE on dof type
 if opt.sysType == "frame"
-x_mask = mod(mu2 - 1, 3) == 0;
-y_mask = mod(mu2 - 1, 3) == 1;
-theta_mask = mod(mu2 - 1, 3) == 2;
-RMSE_dof = [mean(RMSE(x_mask)),mean(RMSE(y_mask)),mean(RMSE(theta_mask))];
-dof_order = ["x","y","$\theta$"];
+    x_enum = mod(mu2 - 1, 3) == 0;
+    y_enum = mod(mu2 - 1, 3) == 1;
+    theta_enum = mod(mu2 - 1, 3) == 2;
+    RMSE_dof = [mean(RMSE(x_enum)),mean(RMSE(y_enum)),mean(RMSE(theta_enum))];
+    dof_order = ["x","y","$\theta$"];
 
-if opt.plot == 1
-figure()
-bar(dof_order,RMSE_dof,'k')
-ylabel('RMSE')
-xlabel('DOF type')
-grid
-title('RMSE DOF type')
-ax = gca; 
-ax.XAxis.TickLabelInterpreter = 'latex';
-end
+    if opt.plot == 1
+        figure()
+        bar(dof_order,RMSE_dof,'k')
+        ylabel('RMSE')
+        xlabel('DOF type')
+        grid
+        title('RMSE DOF type')
+        ax = gca;
+        ax.XAxis.TickLabelInterpreter = 'latex';
+    end
 end
 
 %% Visualization
@@ -317,11 +328,15 @@ if opt.plot == 1
         xlabel('Time [s]')
         ylabel(sprintf('Output (%d)', opt.out_type));
         xlim([0 N*dt])
+
     end
 end
 
 
 % Animate displacements
-if opt.animate == 1 && opt.out_type == 0 && opt.sysType == "frame"
+if opt.animate == 1 && opt.sysType == "frame"
     beamStrucDisplacement(y_est,u,in_dof,opt)
+elseif  opt.animate == 1 && opt.sysType == "chain"
+     chain_animation(y_est,t,N,opt)
 end
+
