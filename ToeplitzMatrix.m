ffffac4ce529d1398d182b1d5f03c8c58edbb61f
MagnusSejerLind <magnussejerlind@gmail.com>
2025-05-22 01:21:12 +0200

    Update ToeplitzMatrix.m

--- ToeplitzMatrix.m
+++ ToeplitzMatrix.m
@@ -1,7 +1,7 @@
 function [H_N] = ToeplitzMatrix(N,ms,r,Ad,Bd,Cd,Dd)
 % Computes the to Toeplitz matrix
 % 
-% N: # of timesteps
+% N: no. of timesteps
 % ms: no. of output dof
 % r: no. of input dof
 % Ad,Bd,Cd,Dd: SS system matricies
@@ -11,7 +11,7 @@ for jj = 1:N
     if jj == 1
         H_N((jj-1)*ms+1:jj*ms,1:jj*r)=[Dd H_N];
     else
-        Q=Cd*Ad^((jj-1)-1)*Bd;
-        H_N((jj-1)*ms+1:jj*ms,1:jj*r)=[Q H_N((jj-2)*ms+1:(jj-1)*ms,:)];
+        Q = Cd*Ad^((jj-1)-1)*Bd;
+        H_N((jj-1)*ms+1:jj*ms,1:jj*r) = [Q H_N((jj-2)*ms+1:(jj-1)*ms,:)];
     end
 end
